d90e72e5698f78d56ea26df72998601bd6efd8f8
LiTao <41500050@qq.com>
2017-05-22 23:50:53 +0800

    测试idea提交2

--- src/main/java/com/myfirst/test.java
+++ src/main/java/com/myfirst/test.java
@@ -17,6 +17,7 @@ public class test {
 
 		DecimalFormat decimalFormat = new DecimalFormat("0.00");
 		System.out.println("测试git修改");
+		System.out.println("测试git修改");
 		System.out.println(0.0D/0.0D*100);
 		System.out.println(decimalFormat.format(0.0D/0.0D*100));
 	}
